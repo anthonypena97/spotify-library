eff728f18dbfbb064d9f7cec8a51aeeb2184d521
Anthony Pena <apena5@alumni.jh.edu>
2021-07-27 15:11:26 -0400

    security

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ const SpotifyWebApi = require('spotify-web-api-node');
 const app = express()
 const port = 8889
 
+require('dotenv').config()
+
+var accessToken = process.env.ACCESS;
+
+
 // // credentials are optional
 var spotifyApi = new SpotifyWebApi({
     clientId: '47002157dbc149a2a3ea1ccacd978bd3',
     clientSecret: '818e5aba42ee47b69482fd4acd4c73cb',
     redirectUri: 'http://localhost:8888/callback/'
 });
-
-var accessToken = 'BQDxNjVAp7N63IfBHZDHtsGaHkrU8aRgnAegEYuHjOGxv0GB-ayNSR01mlb5-e1OrjvJ7Vg-16RSzY3Y_0PPZe7D8DrS_enGmjtGP_3ExtR_rkCC4Ot7t_ncLP9qgAgbfX70uaYF4lazAXcHmfkH8Wz0nBQEzhyREHCZaa5vAA'
 
 spotifyApi.setAccessToken(accessToken);
 
