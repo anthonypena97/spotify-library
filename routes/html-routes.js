cbded8c44d6be36126834a781b8e019298616a6e
Anthony Pena <apena5@alumni.jh.edu>
2021-08-03 11:15:15 -0400

    added library handlebars added google icons

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,18 +11,18 @@ router.get('/search', (req, res) => {
 })
 
 // CREATE ACCOUNT PAGE
-router.get('/create-account', function (req, res) {
-
+router.get('/login', function (req, res) {
+    res.render('login');
 });
 
 // PLAYLIST LIBRARY PAGE
-router.get('/playlist-library', function (req, res) {
-    res.render('playlist');
+router.get('/library', function (req, res) {
+    res.render('library');
 });
 
 // PLAYLIST DISPLAY PAGE
-router.get('/playlist-display', function (req, res) {
-
+router.get('/playlist', function (req, res) {
+    res.render('playlist');
 });
 
 module.exports = router;
